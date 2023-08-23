dcdc64ef836488fe3bd9908210170aa1c035e8af
hyuni0316 <hyunini0408@gmail.com>
2023-08-23 17:40:00 +0900

    no message

---
+++ voca/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import styles from "./App.module.css";
+import Hello from "./component/Hello";  
+
+function App() {
+  return (
+    <div className="App">
+      <Hello age={10} />
+      <Hello age={20} />
+      <Hello age={30} />
+    </div>
+  );
+}
+
+export default App;
