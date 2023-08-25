efdd978afd777c05e7209bd6cbf4f33f38aeb9f4
hyuni0316 <hyunini0408@gmail.com>
2023-08-25 17:25:10 +0900

    10.

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,12 +1,27 @@
-import DayList from "./component/DayList";
 import Header from "./component/Header";
+import DayList from "./component/DayList";
+import Day from "./component/Day";
+import EmptyPage from "./component/EmptyPage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className="App">
       <Header />
-      <DayList /> 
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <DayList /> 
+        </Route>
+        <Route path="/day/:day"> 
+          <Day /> 
+        </Route>
+        <Route>
+          <EmptyPage />
+        </Route>
+      </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- voca/src/component/Header.js
+++ voca/src/component/Header.js
@@ -1,7 +1,9 @@
+import {Link} from "react-router-dom";
+
 export default function Header() {
     return <div className = "header">
         <h1> 
-            <a href="/"> 📒 토익 영단어(고급)</a>
+            <Link to="/"> 📒 토익 영단어(고급)</Link>
         </h1>
         <div className="menu">
             <a href="#x" className="link">
