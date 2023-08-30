1c18067c6ff5c451446510714e764d7d1d8ccdb2
hyuni0316 <hyunini0408@gmail.com>
2023-08-30 17:51:45 +0900

    강의 13까지

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,21 +1,18 @@
-import dummy from "../db/data.json"
 import {useParams} from "react-router-dom";
+import useFetch from '../hooks/useFetch';
+import Word from "./Word";
 
 export default function Day() {
-    // dummy.words
     const {day} =  useParams();
-    const wordList =  dummy.words.filter(word => word.day === Number(day));
+    const words = useFetch(`http://localhost:3001/words?day=${day}`)
 
     return (
         <>
             <h2>Day {day}</h2>
             <table>
                 <tbody>
-                    {wordList.map(word => (
-                        <tr> 
-                            <td> {word.eng} </td>
-                            <td> {word.kor} </td>
-                        </tr>
+                    {words.map(word => (
+                        <Word word={word} key={word.id}/>
                     ))}
                 </tbody>
             </table>
